98f70bfa2a64cc6dff2c533f41159ad178807881
Baron <qbmrodrigo@gmail.com>
2023-09-21 04:18:01 +0800

    Remove services content

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,26 +6,7 @@ import { TableModule } from 'primeng/table';
   selector: 'app-services',
   standalone: true,
   imports: [CommonModule, TableModule],
-  template: `
-    <p-table [value]="products">
-      <ng-template pTemplate="header">
-        <tr>
-          <th>Code</th>
-          <th>Name</th>
-          <th>Category</th>
-          <th>Quantity</th>
-        </tr>
-      </ng-template>
-      <ng-template pTemplate="body" let-product>
-        <tr>
-          <td>{{ product.code }}</td>
-          <td>{{ product.name }}</td>
-          <td>{{ product.category }}</td>
-          <td>{{ product.quantity }}</td>
-        </tr>
-      </ng-template>
-    </p-table>
-  `,
+  template: ` <p>services works!</p> `,
   styles: [],
 })
 export class ServicesComponent {
